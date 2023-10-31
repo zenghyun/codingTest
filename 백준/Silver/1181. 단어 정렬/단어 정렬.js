cc7adb4ca58bcfb37a3ffb7315509cdda9206efc
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-01 01:10:32 +0900

    Update 단어 정렬.js

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -9,20 +9,9 @@ for(let i = 1; i <= num; i++) {
 
 arr = [...new Set(arr)]; 
 
-arr.sort((a,b) => {
-    if(a.length !== b.length) {
-        return a.length - b.length
-    } else {
-        if (a<b) {
-            return -1;
-        } else if (a >b) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-})
+arr.sort().sort((a,b) => a.length - b.length); 
 
 for(let x of arr) {
     console.log(x);
 }
+
