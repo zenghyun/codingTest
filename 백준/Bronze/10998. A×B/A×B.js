75e06962fc0941a8b67abfac69c9db3101a2066b
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-01 14:52:43 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,8 @@
+let input = require('fs').readFileSync('dev/stdin').toString().split('\n');
+
+let line = input[0].split(" ");
+
+let a = parseInt(line[0], 10);
+let b = parseInt(line[1], 10);
+
+console.log(a * b);
