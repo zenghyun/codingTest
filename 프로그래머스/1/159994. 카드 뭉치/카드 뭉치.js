4ff0dcab493a807155a2a8de5570b0a8a96b43c0
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-01 13:41:11 +0900

    [level 1] Title: 카드 뭉치, Time: 2.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,13 @@
+function solution(cards1, cards2, goal) {
+    for(let v of goal) {
+        console.log(v);
+        if(v === cards1[0]) {
+            cards1.shift();
+        } else if ( v === cards2[0]) {
+            cards2.shift();
+        } else {
+            return "No";
+        }
+    }
+    return "Yes";
+}
