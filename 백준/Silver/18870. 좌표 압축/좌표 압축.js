f36a5de221e0ab0a627a90db5d5d1a6a568d667f
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-01 19:53:41 +0900

    [Silver II] Title: 좌표 압축, Time: 1648 ms, Memory: 293868 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -1,17 +1,17 @@
 let fs = require("fs");
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let arr = input[1].split(' ').map(Number);
+let arr = input[1].split(" ").map(Number);
 let setArr = [...new Set(arr)].sort((a,b) => a-b);
 let answer = "";
 let map = new Map(); 
 
 for(let i = 0; i < setArr.length; i++) {
-    map.set(setArr[i], i);
-}
-for(let num of arr ) {
-    answer += map.get(num) + " "
+    map.set(setArr[i],i);
 }
 
+for(let num of arr) {
+    answer += map.get(num) + " "
+}
 
 console.log(answer);
