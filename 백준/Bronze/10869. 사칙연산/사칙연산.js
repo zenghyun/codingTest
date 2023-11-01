4eaf949bbb705657c53ad8b845810be91e32221a
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-01 14:52:34 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,12 @@
+let input = require('fs').readFileSync('dev/stdin').toString().split('\n');
+
+let line = input[0].split(" ");
+
+let a = parseInt(line[0],10);
+let b = parseInt(line[1],10);
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(parseInt(a/b,10));
+console.log(parseInt(a%b,10));
