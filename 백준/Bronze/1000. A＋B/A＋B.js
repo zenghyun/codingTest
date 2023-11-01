b4e544720af200d44daf3211af235ae7fb3b2f72
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-01 14:47:45 +0900

    [Bronze V] Title: A+B, Time: 148 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let line = input[0].split(" ");
+
+let a = parseInt(line[0], 10);
+let b = parseInt(line[1], 10);
+
+console.log(a + b);
