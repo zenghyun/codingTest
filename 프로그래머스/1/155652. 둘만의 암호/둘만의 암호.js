4e6ba9098f6b5efae2531e6b3b616f9b47ff8511
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-03 00:28:43 +0900

    [level 1] Title: 둘만의 암호, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,23 +1,19 @@
 function solution(s, skip, index) {
   let alphabet = Array.from({ length: 26 }, (_, i) =>
     String.fromCharCode(i + 97)
-  );
-  [...skip].forEach((v) => {
-    alphabet = alphabet.filter((w) => w !== v);
-  });
+  ).filter(v => !skip.includes(v));
+
   let text = [...s];
   let answer = "";
+
   text.forEach((v) => {
-    console.log(v);
     let num = alphabet.indexOf(v) + index;
     while (num >= alphabet.length) {
       num -= alphabet.length;
     }
-    if (alphabet[num - 1] === "z") {
-      answer += alphabet[0];
-    } else {
-      answer += alphabet[num];
-    }
+    alphabet[num - 1] === "z" ? (num = 0) : (num = num);
+    answer += alphabet[num];
   });
+  
   return answer;
 }
