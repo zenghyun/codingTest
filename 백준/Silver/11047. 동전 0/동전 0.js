c8a3d1b58f21ca602edef1346cf6337569d795e4
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-06 22:31:46 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -4,13 +4,8 @@ let [num, money] = input[0].split(" ").map(Number);
 let cnt = 0;
 
 for (let i = num; i > 0; i--) {
-  if (money >= input[i]) {
     let num = Math.floor(money / input[i]);
     money = money - input[i] * num;
     cnt += num;
-  }
-  if (money === 0) {
-    break;
-  }
 }
 console.log(cnt);
