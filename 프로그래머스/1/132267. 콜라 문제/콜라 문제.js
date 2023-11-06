fea0d64429cfd17bc3fadeca331e7717438d2152
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-06 17:47:20 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -3,15 +3,8 @@ function solution(a, b, n) {
 
   while (n >= a) {
     let num = Math.floor(n / a);
-    console.log(num);
-    console.log(n);
     result += num * b;
-    console.log(result);
-    if (num % 2 !== 0 && num !== 1) {
-    //   result += 1;
-    }
     n = n - num * a + num * b;
-    console.log(n);
   }
   return result;
 }
