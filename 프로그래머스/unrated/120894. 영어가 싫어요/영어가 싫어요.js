6d3fdd140de5811d11b1dbfb00a5e430675906a1
zenghyun <114131063+zenghyun@users.noreply.github.com>
2023-11-07 13:38:03 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,26 +1,7 @@
 function solution(numbers) {
-  let obj = {
-    zero: 0,
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-  };
-  numbers = numbers.split("");
-  let text = "";
-  let answer = "";
-  numbers.forEach((v) => {
-    text += v;
-    if (obj[text] !== undefined) {
-      answer += obj[text];
-      text = "";
-    }
-  });
-
-  return Number(answer);
+    let numStr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    numStr.forEach((str,idx) => {
+        numbers = numbers.replaceAll(str,idx);
+    })
+  return Number(numbers);
 }
